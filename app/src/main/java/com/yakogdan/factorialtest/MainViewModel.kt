d1cd647900e305fc5baa65c3b076308bf6933215
yakogdan <yakogdan@gmail.com>
2023-08-14 20:09:20 +0800

    CoroutineScope and CoroutineContext

--- app/src/main/java/com/yakogdan/factorialtest/MainViewModel.kt
+++ app/src/main/java/com/yakogdan/factorialtest/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.yakogdan.factorialtest
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.math.BigInteger
@@ -14,6 +17,9 @@ class MainViewModel : ViewModel() {
     private val _state: MutableLiveData<State> = MutableLiveData<State>()
     val state: LiveData<State> get() = _state
 
+    private val myCoroutineScope =
+        CoroutineScope(Dispatchers.Default + CoroutineName("My coroutine scope"))
+
     fun calculate(value: String?) {
         _state.value = Progress
         if (value.isNullOrBlank()) {
@@ -21,20 +27,26 @@ class MainViewModel : ViewModel() {
             return
         }
 
-        viewModelScope.launch {
+        myCoroutineScope.launch(Dispatchers.Main) {
             val number = value.toLong()
-            val result = factorial(number)
+            val result = withContext(Dispatchers.Default) {
+                factorial(number)
+            }
             _state.value = Factorial(value = result)
+            Log.d("MainViewModel", coroutineContext.toString())
         }
     }
 
-    private suspend fun factorial(number: Long): String {
-        return withContext(Dispatchers.Default) {
-            var result = BigInteger.ONE
-            for (i in 1..number) {
-                result = result.multiply(BigInteger.valueOf(i))
-            }
-            result.toString()
+    private fun factorial(number: Long): String {
+        var result = BigInteger.ONE
+        for (i in 1..number) {
+            result = result.multiply(BigInteger.valueOf(i))
         }
+        return result.toString()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        myCoroutineScope.cancel()
     }
 }
